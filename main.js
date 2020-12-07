247d5285b6fb03b068de6679adad5141fb04ca35
Cody Wiseman <cwiseman09@yahoo.com>
2020-12-07 13:27:39 -0800

    Cody Modal box and started JS for add entry

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var $entryButton = document.getElementById('add-entry-button');
 
-$entryButton.addEventListener('click', function (event) {
+var $body = document.querySelector('body')
 
+var $modalForm = document.querySelector('div[class="row position"]')
+console.log($modalForm)
+
+$entryButton.addEventListener('click', function (event) {
+  if($body.className === 'row' && $modalForm.className === 'row position hidden') {
+    $body.className = 'row modal-open';
+    $modalForm.className = 'row position';
+  }
 })

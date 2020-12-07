8ee4e65f84c66c1bdfcf832178e54c628dc92def
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-07 17:01:52 -0600

    Lucca week-day-box

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 var $entryButton = document.getElementById('add-entry-button');
 
-var $body = document.querySelector('body')
+var $body = document.querySelector('body');
 
-var $modalForm = document.querySelector('div[class="row position"]')
-console.log($modalForm)
+var $modalForm = document.querySelector('div[class="row position hidden"]');
 
 $entryButton.addEventListener('click', function (event) {
   if($body.className === 'row' && $modalForm.className === 'row position hidden') {
     $body.className = 'row modal-open';
     $modalForm.className = 'row position';
+    $entryButton.className = "hidden";
   }
+})
+
+var $form = document.getElementById("form");
+
+$form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  if ($body.className === "row modal-open" && $modalForm.className === "row position") {
+  $body.className = 'row';
+  $modalForm.className = 'row position hidden';
+  $entryButton.className = "";
+  }
+  $form.reset();
 })

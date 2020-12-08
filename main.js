e610694281a7e6e8b919e9eea407ae265189759d
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-07 18:19:16 -0600

    Lucca Add Values to Entries array

--- main.js
+++ main.js
@@ -15,23 +15,38 @@ $entryButton.addEventListener('click', function (event) {
 var $form = document.getElementById("form");
 
 $form.addEventListener("submit", function(event) {
-  event.preventDefault();
+  // event.preventDefault();
   if ($body.className === "row modal-open" && $modalForm.className === "row position") {
   $body.className = 'row';
   $modalForm.className = 'row position hidden';
   $entryButton.className = "";
   }
-  var $dayOfWeek = document.getElementById('day-of-week')
-  var $time = document.getElementById('time')
-  var $entryDescription = document.getElementById('description')
+
+  var $dayOfWeek = document.getElementById('day-of-week').value;
+  var $time = document.getElementById('time').value;
+  var $entryDescription = document.getElementById('description').value;
+
+  var entry = {
+    dayOfWeek: $dayOfWeek,
+    time: $time,
+    entryDescription: $entryDescription
+  }
+  entries.push(entry);
+
+  var entriesJSON = JSON.stringify(entries);
+  localStorage.setItem('entries-local-storage', entriesJSON)
+
+  $weekDayBoxes.className = 'row align-center height100';
+
   $form.reset();
 })
 
 var $weekDayBoxes = document.getElementById('week-day-boxes')
 
-
 window.addEventListener("DOMContentLoaded", function(event) {
-  if (previousEntriesJSON) {
+  if (entries.length !== 0) {
     $weekDayBoxes.className = 'row align-center height100';
+  } else {
+    $weekDayBoxes.className = 'row align-center height100 hidden';
   }
 })
